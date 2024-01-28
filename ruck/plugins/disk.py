f3741951986f3c9faf6dfdbc2e82e3b0d03c0b49
Charles Short <charles.short@windriver.com>
2024-01-28 17:23:34 -0500

    Finish disk partition
    Signed-off-by: Charles Short <charles.short@windriver.com>

--- ruck/plugins/disk.py
+++ ruck/plugins/disk.py
@@ -4,15 +4,46 @@ Copyright (c) 2024 Wind River Systems, Inc.
 SPDX-License-Identifier: Apache-2.0
 
 """
+import logging
+import shutil
 
+from ruck import exceptions
 from ruck.plugins.base import Base
-
+from ruck.utils import run_command
 
 class DiskPlugin(Base):
     def __init__(self, state, config, workspace):
         self.state = state
-        self.config = config
+        self.config = config.get("actions")["disk"]
+        self.logging = logging.getLogger(__name__)
         self.workspace = workspace
 
+        self.repart = shutil.which("systemd-repart")
+
     def run_actions(self):
-        print("in disk plugin")
+        """Configure the image disk via systemd-repart."""
+        self.logging.info("Configuring disk.")
+        if self.repart is None:
+            raise exceptions.CommandNotFoundError("%s is not found.", self.repart)
+        
+        definitions = self.config.get("definitions", None)
+        if definitions is None:
+            raise exceptions.ConfigError("Partition definitons not found.")
+        image = self.config.get("image", None)
+        if image is None:
+            raise exceptions.ConfigError("Image is not found.")
+        size = self.config.get("size", None)
+        if size is None:
+            raise exceptions.ConfigError("Size is not specified.")
+
+        run_command([
+            self.repart,
+            "--definitions", definitions,
+            "--empty=create",
+            "--size", size,
+            "--dry-run=no",
+            "--discard=no",
+            "--offline=true",
+            "--no-pager",
+            image],
+            cwd=self.workspace)
