c90b92b27cda5dd74838fd1eeb224ee11fa86773
Charles Short <charles.short@windriver.com>
2024-02-05 15:22:26 +0000

    add umount plugin
    Signed-off-by: Charles Short <charles.short@windriver.com>

---
+++ ruck/plugins/umount.py
@@ -0,0 +1,23 @@
+"""
+Copyright (c) 2024 Wind River Systems, Inc.
+
+SPDX-License-Identifier: Apache-2.0
+
+"""
+
+import os
+
+from ruck import exceptions
+from ruck.plugins.base import Base
+from ruck import utils
+
+class UMountPlugin(Base):
+    def __init__(self, state, config, workspace):
+        self.state = state
+        self.config = config
+        self.workspace = workspace
+        self.mount = config.get("actions")["umount"]
+
+    def run_actions(self):
+        rootfs = self.workspace.joinpath("rootfs")
+        utils.umount(rootfs, self.workspace)
